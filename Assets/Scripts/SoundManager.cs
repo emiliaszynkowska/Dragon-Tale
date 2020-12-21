ccfb6e0ce322228d8ee930ae6cbe94f42e6ce752
Emilia <42769841+emiliaszynkowska@users.noreply.github.com>
2020-12-21 14:26:14 +0000

    UI & Sounds

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    
+    public AudioSource audioSource;
+    public AudioClip music;
+    public AudioClip homeMusic;
+    public AudioClip villageMusic;
+    //public AudioClip fightMusic;
+    //public AudioClip bossMusic;
+    //public AudioClip click;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+        audioSource.clip = music;
+    }
+    
+    public void PlayMusic()
+    {
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+    
+    // public void PlayFight()
+    // {
+    //     audioSource.PlayOneShot(fightMusic);
+    // }
+
+}
