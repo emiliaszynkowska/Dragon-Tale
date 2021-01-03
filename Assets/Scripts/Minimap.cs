72dffd05584e171970093013264cfe3696632890
SWNEast <SWNEast@users.noreply.github.com>
2021-01-03 19:48:04 +0000

    Minimap Added
    - Minimap added
    - Follows player as they move
    - Player marker rotates
    - Zoom with X/C
    - More prefabs established

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerMarker;
+    public Camera cam;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        playerMarker.transform.rotation = Quaternion.Euler(0f, 0f, -player.eulerAngles.y + 180);
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            cam.orthographicSize = 25;
+        } 
+        else if (Input.GetKey(KeyCode.C))
+        {
+            cam.orthographicSize = 100;
+        }
+        else
+        {
+            cam.orthographicSize = 50;
+        }
+    }
+}
---
+++ Assets/Scripts/MinimapMarker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapMarker : MonoBehaviour
+{
+    public Transform player;
+    public float diff;
+
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, -player.eulerAngles.y);
+    }
+}
