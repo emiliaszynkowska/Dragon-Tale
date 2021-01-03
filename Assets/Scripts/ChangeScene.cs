01a9d7c9bb18c02494f9cd4d52ea46221acb518b
SWNEast <SWNEast@users.noreply.github.com>
2021-01-03 17:48:07 +0000

    Post-Merge Care
    - Organised asset folders
    - Can travel between scenes
    - Player is now a prefab

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HomeFire
+{
+    public class ChangeScene : MonoBehaviour
+    {
+        public String scene;
+        void OnTriggerEnter(Collider other)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    
+    }
+}
