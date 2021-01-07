e4be3b1a73ae700eae414cbd5b7244399fdd237a
SWNEast <SWNEast@users.noreply.github.com>
2021-01-07 21:34:35 +0000

    Questing Basics
    - Quest manager script set up
    - 1st quest started
    - Items can be picked up
    - Villagers can be interacted with
    - Quest progress tracked

---
+++ Assets/Scripts/DialogPrompt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogPrompt : MonoBehaviour
+{
+    public QuestManager questManager;
+    public TextMeshProUGUI promptText;
+    public int ID;
+
+    private bool active = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (active)
+        {
+            promptText.text = "Press X to talk";
+            promptText.gameObject.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.X) && active)
+        {
+            questManager.NewQuest(ID);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (active) promptText.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -6,4 +6,10 @@ public static class PlayerData
 {
     public static int VillageExit //0 = from home, //1 = from lair
     { get; set; }
+
+    public static int GrandmasStewPart
+    { get; set; }
+
+    public static int MushroomsCollected
+    { get; set; }
 }
